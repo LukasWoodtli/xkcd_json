b946f87105ef15e75254e4b9fdc601561d166da1
woodtluk <woodtluk@e17a30e0-a4a0-48a1-9280-7d2f855b2862>
2013-12-03 16:18:09 +0000

    Python scripts für gelesene xkcd Comics verschoben.
    git-svn-id: https://xp-dev.com/svn/woodtluk-myStuff@555 e17a30e0-a4a0-48a1-9280-7d2f855b2862

---
+++ xkcd_count.py
@@ -0,0 +1,32 @@
+import urllib2
+import json
+import os
+
+THIS_FILES_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+newest_xkcd = 'http://xkcd.com/info.0.json'
+
+record_file = os.path.join(THIS_FILES_DIRECTORY, 'read_xkcd.txt')
+
+response = urllib2.urlopen(newest_xkcd)
+xkcd = response.read()
+
+num_of_xkcds = json.loads(xkcd)['num']
+
+
+with open(record_file, 'r') as f:
+  lines = f.readlines()
+  lines = lines[1:]
+  lines = [i for i in lines if i != '\n']
+  lines = [i.replace('\n','') for i in lines]
+  num = len([i for i in lines if '-' not in i])
+  lines = [i for i in lines if '-' in i]
+  for line in lines:
+    nums = line.split('-')
+    assert len(nums) == 2
+    num += (int(nums[1]) - int(nums[0]) + 1)
+  
+  still_to_read = num_of_xkcds - num
+  print "Read already: %i" % num
+  print "Still to read: %i" % still_to_read
+  print "Percent read: %f" % (num*100/still_to_read)
